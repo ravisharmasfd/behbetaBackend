8287aaf3cd56f2caa954c93466775d4404d7c4cf
webdeveloper510 <118259227+ravisharmasfd@users.noreply.github.com>
2024-11-21 23:38:53 +0530

    initial page added

--- app.js
+++ app.js
@@ -19,8 +19,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "public") });
+});
+app.use('/api', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
